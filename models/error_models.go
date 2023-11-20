c9669cb15d63d2541a21ca35e6d8802ffdb55f92
bibin-zoz <bibinvinodvfx@gmail.com>
2023-11-20 15:57:22 +0530

    toas,alert validations

---
+++ models/error_models.go
@@ -0,0 +1,10 @@
+package models
+
+type Producterror struct {
+	ProductNameError    string
+	ProductDetailsError string
+	StockError          string
+	RamNameError        string
+	CategoryError       string
+	PriceError          string
+}
